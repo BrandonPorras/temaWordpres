115656630ee1bfe1c06097a5557dbbb478a1f1f5
BreandonPorras <brandon.porras.26@gmail.com>
2020-05-27 14:58:26 -0600

    first commit

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="row justify-content-center footer">
+
+    <h5 class="copyright">© 2020 Copyright <a class="link" href="#"><strong>URSART</strong>.com</a></h5>
+
+</footer>
---
+++ index.php
@@ -0,0 +1,33 @@
+<?php get_header();?>
+
+<!-- start slipsum code -->
+
+
+<!-- -->
+
+<body id="fondo-landing">
+
+    <!-- URSART Title-->
+    <div class="row justify-content-center mt-5 pt-5">
+
+        <h1 id="logo-landing" class="mt-5">~ Ursart ~</h1>
+
+    </div>
+
+    <!-- Start Button-->
+    <div class="row justify-content-center mt-3">
+<a ></a>
+        <a class="btn-start" href="http://localhost/wordpress/wordpress/historias/"><strong>Start</strong></a>
+
+    </div>
+
+<footer class="row justify-content-center footer">
+
+    <h5 class="copyright">© 2020 Copyright <a class="link" href="#"><strong>URSART</strong>.com</a></h5>
+
+</footer>
+
+</body>
+<!-- end slipsum code -->
+
+<?php get_footer();?>
